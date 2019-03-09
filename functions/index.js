7f9def7f5613035b3029aa3e3c06f313d3da9f55
Arne Maier <Arne_Maier@gmx.de>
2019-03-09 20:41:14 +0100

    Use firebase

---
+++ functions/index.js
@@ -0,0 +1,32 @@
+const functions = require('firebase-functions');
+
+const firebaseAdmin = require('firebase-admin');
+
+const serviceAccount = require('../serviceAccount.json');
+const invitedUsers = require('../passwd.json').users;
+
+firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+    databaseURL: 'https://marriage-test.firebaseio.com/'
+});
+
+exports.authenticationRequest = functions.https.onRequest((req, res) => {
+    const loginCode = req.query.loginCode;
+    console.log(`Authentication requested with loginCode ${loginCode}`);
+
+    const user = invitedUsers.find(invitation => invitation.loginCode === loginCode);
+    if (user) {
+        const firebaseTokenInfo = {
+            name: user.name,
+        };
+        firebaseAdmin.auth().createCustomToken(user.key, firebaseTokenInfo)
+            .then(customToken => res.json(customToken))
+            .catch(error => {
+                console.error('No customToken', error);
+                res.status(401).send('No customToken');
+            });
+            return;
+    }
+    console.error(`Failed login for ${loginCode}`)
+    res.status(401).send('Not authorized');
+});
