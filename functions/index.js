634bd28061a0dc73dc1ef17320d3216c2a6bb486
Arne Maier <Arne_Maier@gmx.de>
2019-03-09 21:04:15 +0100

    Use cors

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions');
-
 const firebaseAdmin = require('firebase-admin');
+const cors = require('cors')({
+    origin: true,
+});
 
 const serviceAccount = require('./serviceAccount.json');
 const invitedUsers = require('./passwd.json').users;
@@ -11,22 +13,24 @@ firebaseAdmin.initializeApp({
 });
 
 exports.authenticationRequest = functions.https.onRequest((req, res) => {
-    const loginCode = req.query.loginCode;
-    console.log(`Authentication requested with loginCode ${loginCode}`);
+    return cors(req, res, () => {
+        const loginCode = req.query.loginCode;
+        console.log(`Authentication requested with loginCode ${loginCode}`);
 
-    const user = invitedUsers.find(invitation => invitation.loginCode === loginCode);
-    if (user) {
-        const firebaseTokenInfo = {
-            name: user.name,
-        };
-        firebaseAdmin.auth().createCustomToken(user.key, firebaseTokenInfo)
-            .then(customToken => res.json(customToken))
-            .catch(error => {
-                console.error('No customToken', error);
-                res.status(401).send('No customToken');
-            });
-            return;
-    }
-    console.error(`Failed login for ${loginCode}`)
-    res.status(401).send('Not authorized');
+        const user = invitedUsers.find(invitation => invitation.loginCode === loginCode);
+        if (user) {
+            const firebaseTokenInfo = {
+                name: user.name,
+            };
+            firebaseAdmin.auth().createCustomToken(user.key, firebaseTokenInfo)
+                .then(customToken => res.json(customToken))
+                .catch(error => {
+                    console.error('No customToken', error);
+                    res.status(401).send('No customToken');
+                });
+                return;
+        }
+        console.error(`Failed login for ${loginCode}`)
+        res.status(401).send('Not authorized');
+    });
 });
